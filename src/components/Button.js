09f98b5fd924c854c3587c5d8d8c1b945a25531b
Yashpreet Singh <yashpreet1686@gmail.com>
2024-08-01 18:02:24 +0530

    fix buttonlist

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({name}) => {
   return (
-    <button className='rounded-lg bg-gray-100 hover:bg-gray-200 font-semibold text-sm'>{name}</button>
+    <button className='px-3 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 font-semibold text-sm whitespace-nowrap'>{name}</button>
   )
 }
 
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -6,7 +6,7 @@ const ButtonList = () => {
   const List = ["All", "< 5min", "Music", "Mixes", "Source", "Deadpool", "News", "Cloud", "Trailer", "Live", "Indian", "Thoughts", "Strategies", "Startup", "Movies", "New to you", "Watched", "Gadgets", "Products", "Recently Uploaded"]
 
   return (
-    <div className='mt-3 p-2 w-full flex bg-red-900'>
+    <div className='mt-3 p-2 w-full flex gap-x-2 overflow-x-scroll scrollbar-none'>
       {List.map( (item,id) => {
         return <Button key={id} name={item} />
       })}
